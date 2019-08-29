1f47b868bdb1f241ac73b9c20ba144949d8f0982
SyamalaDora <syamala.dora@emc.com>
2019-08-29 15:21:54 +0530

    service-discovery

--- applications/timesheets-server/src/main/java/io/pivotal/pal/tracker/timesheets/App.java
+++ applications/timesheets-server/src/main/java/io/pivotal/pal/tracker/timesheets/App.java
@@ -3,13 +3,15 @@ package io.pivotal.pal.tracker.timesheets;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.client.RestOperations;
 
 import java.util.TimeZone;
 
-
+@EnableEurekaClient
 @SpringBootApplication
 @ComponentScan({"io.pivotal.pal.tracker.timesheets", "io.pivotal.pal.tracker.restsupport"})
 public class App {
@@ -20,6 +22,7 @@ public class App {
     }
 
     @Bean
+    @LoadBalanced
     ProjectClient projectClient(
         RestOperations restOperations,
         @Value("${registration.server.endpoint}") String registrationEndpoint
